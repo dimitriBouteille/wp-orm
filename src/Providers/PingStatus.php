799de0ab5871893e563ed5ce37f67dfe1044f464
dimitri-bouteille <bonjour@dimitri-bouteille.fr>
2023-09-25 22:24:20 +0200

    #6 Add PingStatus & PostStatus

---
+++ src/Providers/PingStatus.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright (c) 2023 Dimitri BOUTEILLE (https://github.com/dimitriBouteille)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Dimitri BOUTEILLE <bonjour@dimitri-bouteille.fr>
+ */
+
+namespace Dbout\WpOrm\Providers;
+
+enum PingStatus: string
+{
+    case  Closed = 'closed';
+    case Open = 'open';
+}
---
+++ src/Providers/PostStatus.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright (c) 2023 Dimitri BOUTEILLE (https://github.com/dimitriBouteille)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Dimitri BOUTEILLE <bonjour@dimitri-bouteille.fr>
+ */
+
+namespace Dbout\WpOrm\Providers;
+
+/**
+ * @see https://wordpress.org/documentation/article/post-status/
+ */
+enum PostStatus: string
+{
+    case Publish = 'publish';
+    case Future = 'future';
+    case Draft = 'draft';
+    case Pending = 'pending';
+    case Private = 'private';
+    case Trash = 'trash';
+    case AutoDraft = 'auto-draft';
+    case Inherit = 'inherit';
+}
