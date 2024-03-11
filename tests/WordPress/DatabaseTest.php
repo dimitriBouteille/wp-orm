f294c56886d005499f668bbca7131558f2fa1eb7
dimitri-bouteille <bonjour@dimitri-bouteille.fr>
2024-03-11 22:47:41 +0100

    dump $wpdb

--- tests/WordPress/DatabaseTest.php
+++ tests/WordPress/DatabaseTest.php
@@ -4,5 +4,9 @@ namespace Dbout\WpOrm\Tests\WordPress;
 
 class DatabaseTest extends \WP_UnitTestCase
 {
-
+    public function set_up()
+    {
+        global $wpdb;
+        var_dump($wpdb);
+    }
 }
