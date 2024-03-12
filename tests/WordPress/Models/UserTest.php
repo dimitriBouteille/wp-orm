9685078e71fe625cfeed68f2e84ca05f30bfccf1
dimitri-bouteille <bonjour@dimitri-bouteille.fr>
2024-03-12 23:08:12 +0100

    Start UserTest.php

--- tests/WordPress/Models/UserTest.php
+++ tests/WordPress/Models/UserTest.php
@@ -9,13 +9,30 @@ use Dbout\WpOrm\Models\User;
  */
 class UserTest extends \WP_UnitTestCase
 {
+    private const USER_EMAIL = 'wp-testing@wp-orm.fr';
+    private const USER_LOGIN = 'testing.wp-orm';
+    private ?int $testingUserId = null;
+
+    /**
+     * @return void
+     */
+    public function setUpBeforeClass(): void
+    {
+        $this->testingUserId = wp_insert_user([
+            'user_login' => self::USER_LOGIN,
+            'user_pass'  => 'testing',
+            'user_email' => self::USER_EMAIL
+        ]);
+    }
+
     /**
      * @return void
      * @covers ::findOneByEmail
      */
-    public function testFindOneByLogin(): void
+    public function testFindOneByEmail(): void
     {
-        $user = User::findOneByEmail('test@test.fr');
+        $user = User::findOneByEmail(self::USER_EMAIL);
         $this->assertInstanceOf(User::class, $user);
+        $this->assertEquals($this->testingUserId, $user->getId());
     }
 }
