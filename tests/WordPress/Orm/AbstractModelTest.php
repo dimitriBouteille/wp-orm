c070533d143d2ebf0d4f9b91d45a4afe6078188d
dimitri-bouteille <bonjour@dimitri-bouteille.fr>
2024-03-16 12:21:40 +0100

    WIP AbstractModelTest.php

--- tests/WordPress/Orm/AbstractModelTest.php
+++ tests/WordPress/Orm/AbstractModelTest.php
@@ -8,8 +8,65 @@
 
 namespace Dbout\WpOrm\Tests\WordPress\Orm;
 
+use Dbout\WpOrm\Models\Article;
 use Dbout\WpOrm\Tests\WordPress\TestCase;
+use Illuminate\Database\QueryException;
 
+/**
+ * @coversDefaultClass \Dbout\WpOrm\Orm\AbstractModel
+ */
 class AbstractModelTest extends TestCase
 {
+    /**
+     * @param string $saveMethod
+     * @return void
+     * @covers ::save
+     * @covers ::saveOrFail
+     * @dataProvider providerTestSave
+     */
+    public function testSuccessSave(string $saveMethod): void
+    {
+        $model = new Article();
+        $model->$saveMethod();
+
+        $expectedId = $model->getId();
+        $this->assertIsNumeric($expectedId);
+
+        $expectedModel = get_post($expectedId);
+        $this->assertInstanceOf(\WP_Post::class, $expectedModel);
+        $this->assertEquals($expectedId, $expectedModel->ID);
+    }
+
+    /**
+     * @param string $saveMethod
+     * @return void
+     * @covers ::save
+     * @covers ::saveOrFail
+     * @dataProvider providerTestSave
+     */
+    public function testSaveWithInvalidProperty(string $saveMethod): void
+    {
+        $model = new Article([
+            'custom_column' => '15',
+        ]);
+
+        // Maybe more strict ?
+        // Check this message: Unknown column 'custom_column' in 'field list'
+        $this->expectException(QueryException::class);
+        $model->$saveMethod();
+    }
+
+    /**
+     * @return \Generator
+     */
+    protected function providerTestSave(): \Generator
+    {
+        yield 'With save function' => [
+            'save',
+        ];
+
+        yield 'With saveOrFail function' => [
+            'saveOrFail',
+        ];
+    }
 }
