d5c819669f7b8c1a26a147433dff7da13df15661
Dimitri BOUTEILLE <bonjour@dimitri-bouteille.fr>
2024-05-20 11:04:54 +0200

    Replace wp-test-config.php by wp-tests-config-sample.php

--- wp-tests-config-sample.php
+++ wp-tests-config-sample.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ * @see https://github.com/rnagle/wordpress-unit-tests/blob/master/wp-tests-config-sample.php
+ */
 define('DB_NAME', 'wordpress_test');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
