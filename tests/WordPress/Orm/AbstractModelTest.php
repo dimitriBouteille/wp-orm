0ce4ba73f45c3e550c421efdc5c95117bde0d8db
Dimitri BOUTEILLE <bonjour@dimitri-bouteille.fr>
2024-05-20 11:09:23 +0200

    Update tests

--- tests/WordPress/Orm/AbstractModelTest.php
+++ tests/WordPress/Orm/AbstractModelTest.php
@@ -228,8 +228,8 @@ class AbstractModelTest extends TestCase
         $this->assertInstanceOf(Option::class, $option);
         $this->assertEquals($expectedValue, $option->getOptionValue());
 
-        wp_cache_delete($optionName, 'options');
-        $wpOpt = get_option($optionName);
+        \wp_cache_delete($optionName, 'options');
+        $wpOpt = \get_option($optionName);
         $this->assertEquals($expectedValue, $wpOpt);
     }
 }
--- wp-tests-config-sample.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @see https://github.com/rnagle/wordpress-unit-tests/blob/master/wp-tests-config-sample.php
- */
-define('DB_NAME', 'wordpress_test');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', '127.0.0.1:3306');
-define('WP_CACHE', false);
