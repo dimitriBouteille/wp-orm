9457ac0a98cbe49d4c919762bd25f375530bc759
Dimitri BOUTEILLE <bonjour@dimitri-bouteille.fr>
2024-10-13 21:52:09 +0200

    Add DatabaseTransactionTest

---
+++ tests/WordPress/Orm/DatabaseTransactionTest.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Copyright © Dimitri BOUTEILLE (https://github.com/dimitriBouteille)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Dimitri BOUTEILLE <bonjour@dimitri-bouteille.fr>
+ */
+
+namespace Dbout\WpOrm\Tests\WordPress\Orm;
+
+use Dbout\WpOrm\Orm\AbstractModel;
+use Dbout\WpOrm\Orm\Database;
+use Dbout\WpOrm\Tests\WordPress\TestCase;
+
+class DatabaseTransactionTest extends TestCase
+{
+    private static string $tableName = '';
+    private static AbstractModel $model;
+
+
+    /**
+     * @return void
+     */
+    public static function setUpBeforeClass(): void
+    {
+        global $wpdb;
+
+        $tableName = $wpdb->prefix . 'document';
+        self::$tableName = $tableName;
+        $sql = "CREATE TABLE $tableName (
+            id INT NOT NULL AUTO_INCREMENT,
+            name varchar(100) NOT NULL,
+            url varchar(55) DEFAULT '' NOT NULL
+            PRIMARY KEY  (id)
+        );";
+
+        require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+        dbDelta($sql);
+
+        self::$model = new class () extends AbstractModel {
+            protected $primaryKey = 'id';
+            public $timestamps = false;
+            protected $table = 'document';
+        };
+
+        define('SAVEQUERIES', true);
+    }
+
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        self::$model::truncate();
+    }
+
+    /**
+     * @throws \Throwable
+     * @return void
+     * @covers Database::transaction
+     */
+    public function testTransactionSuccess(): void
+    {
+        Database::getInstance()->transaction(function () {
+            Database::getInstance()->insert(self::$tableName, [
+                'name' => 'Invoice #15',
+                'url' => 'invoice-15',
+            ]);
+            Database::getInstance()->insert(self::$tableName, [
+                'name' => 'Invoice #10',
+                'url' => 'invoice-10',
+            ]);
+        });
+
+        global $wpdb;
+        var_dump($wpdb->queries);
+    }
+}
