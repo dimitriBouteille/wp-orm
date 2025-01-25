43ba422621dc92f781fbf6b27e7af81bbb341108
Dimitri BOUTEILLE <bonjour@dimitri-bouteille.fr>
2025-01-25 18:41:30 +0100

    🔧 Add tests PrunableTest

---
+++ tests/WordPress/Concerns/PrunableTest.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Copyright © Dimitri BOUTEILLE (https://github.com/dimitriBouteille)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Dimitri BOUTEILLE <bonjour@dimitri-bouteille.fr>
+ */
+
+namespace Dbout\WpOrm\Tests\WordPress\Concerns;
+
+use Carbon\Carbon;
+use Dbout\WpOrm\Orm\Database;
+use Dbout\WpOrm\Tests\WordPress\TestCase;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
+use Illuminate\Database\Schema\Blueprint;
+
+class PrunableTest extends TestCase
+{
+    public static function setUpBeforeClass(): void
+    {
+        Database::getInstance()->getSchemaBuilder()->create('sales_payment', function (Blueprint $table) {
+            $table->id();
+            $table->date('created_at');
+            $table->string('method');
+            $table->float('amount');
+        });
+    }
+
+    /**
+     * @return void
+     * @covers Prunable::prunable()
+     */
+    public function testPrunable(): void
+    {
+        $model = new class () extends Model {
+            use Prunable;
+
+            protected $table = 'sales_payment';
+
+            public function prunable()
+            {
+                return static::where('created_at', '<', Carbon::create(2025, 1, 1));
+            }
+        };
+
+        $values = [
+            [
+                'method' => 'adyen',
+                'amount' => 100,
+                'created_at' => Carbon::create(2024, 05, 10),
+            ],
+            [
+                'method' => 'paypal',
+                'amount' => 50.52,
+                'created_at' => Carbon::create(2023, 10, 8),
+            ],
+            [
+                'method' => 'apple_pay',
+                'amount' => 145.12,
+                'created_at' => Carbon::create(2024, 5, 28),
+            ],
+            [
+                'method' => 'amazon_pay',
+                'amount' => 199.99,
+                'created_at' => Carbon::create(2025, 4, 15),
+            ],
+            [
+                'method' => 'swiss_pay',
+                'amount' => 129.50,
+                'created_at' => Carbon::create(2025, 3, 5),
+            ],
+            [
+                'method' => 'adyen',
+                'amount' => 15.99,
+                'created_at' => Carbon::create(2024, 7, 19),
+            ],
+            [
+                'method' => 'ideal',
+                'amount' => 69.10,
+                'created_at' => Carbon::create(2026, 4, 5),
+            ],
+        ];
+
+        $model::insert($values);
+        $result = (new $model())->pruneAll();
+
+        $this->assertEquals(4, $result);
+    }
+}
